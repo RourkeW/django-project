4518aebe8d4a6adaf0f144d670f73df4baae0373
Rourke Whitcombe <rourke28whit@gmail.com>
2024-10-01 16:26:26 +0000

    feat: I have added some files essential for the project to work on heroku

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xam(sy7^#3jzre*ried1407@^v6v5ereh8#k!07jsxcqocusdk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-rourkew-djangoproject-dgtkpwzoqql.ws-eu116.gitpod.io']
+ALLOWED_HOSTS = ['8000-rourkew-djangoproject-dgtkpwzoqql.ws-eu116.gitpod.io',
+                '.herokuapp.com']
 
 
 # Application definition
